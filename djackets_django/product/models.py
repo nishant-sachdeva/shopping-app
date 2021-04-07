d2226ef086cb0655cc782b794908dac7543bf2a2
nishant-sachdeva <nishant.sachdeva@research.iiit.ac.in>
2021-04-07 18:03:20 +0530

    Front page has been made

--- djackets_django/product/models.py
+++ djackets_django/product/models.py
@@ -44,19 +44,19 @@ class Product(models.Model):
 
 	def get_image(self):
 		if self.image:
-			return 'http://localhost:8000/' + self.image.url 
+			return 'http://localhost:8000' + self.image.url 
 		return ''
 
 
 	def get_thumbnail(self):
 		if self.thumbnail:
-			return 'http://localhost:8000/' + self.thumbnail.url 
+			return 'http://localhost:8000' + self.thumbnail.url 
 		else:
 			if self.image:
 				self.thumbnail = self.make_thumbnail(self.image)
 				self.save()
 
-				return 'http://localhost:8000/' + self.thumbnail.url 
+				return 'http://localhost:8000' + self.thumbnail.url 
 			else:
 				return ''
 
